7f577d34138073efb818fe95eb1fe68369fa32fa
RybkinMike <Misha.tes-03@mail.ru>
2023-06-28 21:25:23 +0300

    fix: ревью от 27.06 1

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/StatsRepository.java
@@ -13,11 +13,11 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
             "from Stats as s")
     List<String> getDistinctUri();
 
-    @Query(value = "select s.uri " +
-            "from stats as s " +
+    @Query(value = "select t.uri " +
+            "from ( select distinct on (s.ip) s.uri from stats as s " +
             "where s.uri in (?1) " +
-            "and s.created > ?2 and s.created < ?3 " +
-            "group by s.ip", nativeQuery = true)
+            "and s.created > ?2 and s.created < ?3) as t " +
+            "", nativeQuery = true)
     List<String> getUrisByUriForUniqueIP(List<String> uris, LocalDateTime from, LocalDateTime to);
 
     @Query("select s.uri " +
